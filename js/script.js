f7334171340f81c6082776c89d4e4d422e4c0d0b
Vasin Andrey <vofus@yandex.ru>
2015-08-04 03:45:34 +0400

    Init  commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	var touch = $('#touch-menu');
+	var menu = $('.menu');
+
+	$(touch).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+	$(window).resize(function() {
+		var wid = $(window).width();
+		if (wid > 760 && menu.is(':hidden')){
+			menu.removeAttr('style');
+		}
+	});
+});
